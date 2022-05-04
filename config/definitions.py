0dde25b3a03d7c913ae1ffdffdae5a638f0934ab
Grégory Ployart <gregory.ployart@gmail.com>
2022-05-04 22:21:45 +0200

    definitions.py correction

--- config/definitions.py
+++ config/definitions.py
@@ -3,12 +3,13 @@ import ctypes.wintypes
 
 
 ROOT_DIR = os.path.realpath(os.path.join(os.path.dirname(__file__), '..'))
+main_path = os.path.join(ROOT_DIR, 'main.py')
 src_dir = os.path.join(ROOT_DIR, 'src')
 app_dir = os.path.join(src_dir, 'app')
-main_path = os.path.join(app_dir, 'ui', 'Main.py')
 assets_dir = os.path.join(src_dir, 'assets')
+data_dir = os.path.join(assets_dir, 'data')
 img_dir = os.path.join(assets_dir, 'img')
-txt_dir = os.path.join(assets_dir, 'txt')
+#txt_dir = os.path.join(assets_dir, 'txt')
 
 CSIDL_PERSONAL = 5
 SHGFP_TYPE_CURRENT = 0
